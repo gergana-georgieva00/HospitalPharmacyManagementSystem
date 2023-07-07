af799b2712259a0ae763b4305deffc58bd220d87
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-08 00:32:38 +0300

    Implemented Pharmacist Sevice

---
+++ HospitalPharmacyManagementSystem.Services.Data/Interfaces/IPharmacistService.cs
@@ -0,0 +1,7 @@
+﻿namespace HospitalPharmacyManagementSystem.Services.Data.Interfaces
+{
+    public interface IPharmacistService
+    {
+        Task<bool> PharmacistExistsByUserId(string userId);
+    }
+}
---
+++ HospitalPharmacyManagementSystem.Services.Data/PharmacistService.cs
@@ -0,0 +1,24 @@
+﻿using HospitalPharmacyManagementSystem.Services.Data.Interfaces;
+using HospitalPharmacyManagementSystem.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalPharmacyManagementSystem.Services.Data
+{
+    public class PharmacistService : IPharmacistService
+    {
+        private readonly HospitalPharmacyManagementSystemDbContext dbContext;
+
+        public PharmacistService(HospitalPharmacyManagementSystemDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<bool> PharmacistExistsByUserId(string userId)
+        {
+            bool result = await this.dbContext.Pharmacists
+                .AnyAsync(p => p.UserId.ToString() == userId);
+
+            return result;
+        }
+    }
+}

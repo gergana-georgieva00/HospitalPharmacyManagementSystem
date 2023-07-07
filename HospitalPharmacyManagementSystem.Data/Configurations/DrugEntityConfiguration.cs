15aada8286b3e3890b2f9d1275a643d1e784b4e1
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-07 14:59:04 +0300

    Implemented DrugEntity Configuration

--- HospitalPharmacyManagementSystem.Data/Configurations/DrugEntityConfiguration.cs
+++ HospitalPharmacyManagementSystem.Data/Configurations/DrugEntityConfiguration.cs
@@ -39,7 +39,31 @@
             };
             drugs.Add(drug);
 
+            drug = new Drug()
+            {
+                BrandName = "Lipitor",
+                Form = Common.Enums.DrugForm.Tablet,
+                Description = "LIPITOR is a prescription medicine that contains a cholesterol " +
+                "lowering medicine (statin) called atorvastatin.",
+                ImageUrl = "https://pharmacy.ansvel.com.ng/wp-content/uploads/sites/10/2016/03/lipitor-_atorvastatin-calcium_-10mg-x30-tabs-1_fxljg4_500x.webp",
+                Price = 20.71m,
+                CategoryId = 2,
+                PharmacistId = Guid.Parse("1B65AAD4-F701-4209-84DB-746688809C34")
+            };
+            drugs.Add(drug);
 
+            drug = new Drug()
+            {
+                BrandName = "Nature Made Fish Oil",
+                Form = Common.Enums.DrugForm.Capsule,
+                Description = "The Omega-3 fatty acids (EPA and DHA) found in fish " +
+                "and other marine life help support a healthy heart.",
+                ImageUrl = "https://www.bioshop.bg/images/detailed/10/nature-way-fish-oil.jpg",
+                Price = 24.99m,
+                CategoryId = 3,
+                PharmacistId = Guid.Parse("1B65AAD4-F701-4209-84DB-746688809C34")
+            };
+            drugs.Add(drug);
 
             return drugs.ToArray();
         }

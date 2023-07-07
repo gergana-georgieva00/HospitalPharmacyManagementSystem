9c93fcd49814a90355a17857a4d1424f5608b0af
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-07 22:57:03 +0300

    Added BecomePharmacist controller

---
+++ HospitalPharmacyManagementSystem.Web/Controllers/PharmacistController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalPharmacyManagementSystem.Web.Controllers
+{
+    [Authorize]
+    public class PharmacistController : Controller
+    {
+        public async Task<IActionResult> BecomePharmacist()
+        {
+            return View();
+        }
+    }
+}

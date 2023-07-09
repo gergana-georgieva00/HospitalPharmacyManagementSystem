80146cff8f335d807acaf0c3ba6804512635c7a7
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-09 21:16:11 +0300

    Added notification messages

---
+++ HospitalPharmacyManagementSystem.Common/NotificationMessages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalPharmacyManagementSystem.Common
+{
+    public class NotificationMessages
+    {
+        public const string ErrorMessage = "Error";
+        public const string WarningMessage = "Warning";
+        public const string InformationMessage = "Information";
+        public const string SuccessMessage = "Success";
+    }
+}

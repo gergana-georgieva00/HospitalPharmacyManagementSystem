8b8dd656f44d44d15d6a2725247cd836498612a3
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-24 13:53:03 +0300

    Added DrugSorting enum

---
+++ HospitalPharmacyManagementSystem.Web.ViewModels/Drug/Enums/DrugSorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalPharmacyManagementSystem.Web.ViewModels.Drug.Enums
+{
+    public enum DrugSorting
+    {
+        Newest = 0,
+        Oldest = 1,
+        PriceAscending = 2,
+        PriceDescending = 3
+    }
+}

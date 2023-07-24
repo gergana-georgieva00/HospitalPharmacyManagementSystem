ca3e8081dd023e26f2852c3883f11f33ff8a17f8
Gergana Georgieva <gerganagergana1@gmail.com>
2023-07-24 14:13:29 +0300

    Added AllDrugsFilteredAndPagedServiceModel

---
+++ HospitalPharmacyManagementSystem.Services.Data.Models/Drug/AllDrugsFilteredAndPagedServiceModel.cs
@@ -0,0 +1,10 @@
+﻿using HospitalPharmacyManagementSystem.Web.ViewModels.Drug;
+
+namespace HospitalPharmacyManagementSystem.Services.Data.Models.Drug
+{
+    public class AllDrugsFilteredAndPagedServiceModel
+    {
+        public int TotalDrugsCount { get; set; }
+        public IEnumerable<DrugAllViewModel> Drugs { get; set; } = new HashSet<DrugAllViewModel>();
+    }
+}

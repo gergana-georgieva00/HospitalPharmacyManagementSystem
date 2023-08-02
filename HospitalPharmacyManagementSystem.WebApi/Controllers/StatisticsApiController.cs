0f3a7cbab851d4b8cd436c0c67c6b4affb786e11
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-02 12:33:27 +0300

    Implemented GetStatistics()

--- HospitalPharmacyManagementSystem.WebApi/Controllers/StatisticsApiController.cs
+++ HospitalPharmacyManagementSystem.WebApi/Controllers/StatisticsApiController.cs
@@ -1,10 +1,35 @@
 ﻿namespace HospitalPharmacyManagementSystem.WebApi.Controllers
 {
+    using HospitalPharmacyManagementSystem.Services.Data.Interfaces;
+    using HospitalPharmacyManagementSystem.Services.Data.Models.Statistics;
     using Microsoft.AspNetCore.Mvc;
 
     [Route("api/statistics")]
     [ApiController]
     public class StatisticsApiController : ControllerBase
     {
+        private readonly IDrugService drugService;
+
+        public StatisticsApiController(IDrugService drugService)
+        {
+            this.drugService = drugService;
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(200, Type = typeof(StatisticsServiceModel))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetStatistics()
+        {
+            try
+            {
+                StatisticsServiceModel model = await this.drugService.GetStatisticsAsync();
+                return this.Ok(model);
+            }
+            catch (Exception)
+            {
+                return this.BadRequest();
+            }
+        }
     }
 }

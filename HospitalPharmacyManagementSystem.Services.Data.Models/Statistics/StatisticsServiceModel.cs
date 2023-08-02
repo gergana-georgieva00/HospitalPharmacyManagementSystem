06f2f922fa8b7041cb378840b534ae75c23c6d4c
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-02 12:08:23 +0300

    Added StatisticsServiceModel

---
+++ HospitalPharmacyManagementSystem.Services.Data.Models/Statistics/StatisticsServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalPharmacyManagementSystem.Services.Data.Models.Statistics
+{
+    internal class StatisticsServiceModel
+    {
+    }
+}

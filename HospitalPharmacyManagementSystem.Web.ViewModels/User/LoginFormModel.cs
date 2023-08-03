2d0b85c2af07cc666990a5d51bd3a248ad998f91
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-03 21:49:54 +0300

    Implemented login.cshtml

--- HospitalPharmacyManagementSystem.Web.ViewModels/User/LoginFormModel.cs
+++ HospitalPharmacyManagementSystem.Web.ViewModels/User/LoginFormModel.cs
@@ -1,19 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Xml.Linq;
-
-namespace HospitalPharmacyManagementSystem.Web.ViewModels.User
+﻿namespace HospitalPharmacyManagementSystem.Web.ViewModels.User
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class LoginFormModel
     {
         [Required]
         [EmailAddress]
-        public string Email { get; set; }
+        public string Email { get; set; } = null!;
 
         [Required]
         [DataType(DataType.Password)]
-        public string Password { get; set; }
+        public string Password { get; set; } = null!;
 
         [Display(Name = "Remember me?")]
         public bool RememberMe { get; set; }
+        public string? ReturnUrl { get; set; }
     }
 }

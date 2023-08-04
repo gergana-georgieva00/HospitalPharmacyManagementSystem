41324cdbc77241dd13a599f1114a0f99af37ceee
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-04 13:51:40 +0300

    added IsAdmin()

--- HospitalPharmacyManagementSytem.Web.Infrastucture/Extensions/ClaimsPrincipleExtentions.cs
+++ HospitalPharmacyManagementSytem.Web.Infrastucture/Extensions/ClaimsPrincipleExtentions.cs
@@ -1,12 +1,18 @@
-﻿using System.Security.Claims;
-
-namespace HospitalPharmacyManagementSytem.Web.Infrastucture.Extensions
+﻿namespace HospitalPharmacyManagementSytem.Web.Infrastucture.Extensions
 {
+    using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+    using System.Security.Claims;
+    using static HospitalPharmacyManagementSystem.Common.GeneralAppConstants;
     public static class ClaimsPrincipleExtensions
     {
         public static string? GetId(this ClaimsPrincipal user)
         {
             return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+        public static bool IsAdmin(this ClaimsPrincipal user)
+        {
+            return user.IsInRole(AdminRoleName);
         }
     }
 }

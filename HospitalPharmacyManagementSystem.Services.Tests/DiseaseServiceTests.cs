87c5ce08da5af1997d16320a733de15cb31d6118
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-19 15:07:08 +0300

    configured DiseaseService tests

---
+++ HospitalPharmacyManagementSystem.Services.Tests/DiseaseServiceTests.cs
@@ -0,0 +1,30 @@
+﻿namespace HospitalPharmacyManagementSystem.Services.Tests
+{
+    using HospitalPharmacyManagementSystem.Data;
+    using HospitalPharmacyManagementSystem.Services.Data.Interfaces;
+    using HospitalPharmacyManagementSystem.Services.Data;
+    using Microsoft.EntityFrameworkCore;
+    using static DatabaseSeeder;
+
+    public class DiseaseServiceTests
+    {
+        private HospitalPharmacyManagementSystemDbContext dbContext;
+        private DbContextOptions<HospitalPharmacyManagementSystemDbContext> dbOptions;
+        private IDiseaseService diseaseService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<HospitalPharmacyManagementSystemDbContext>()
+                .UseInMemoryDatabase("HospitalPharmacyInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new HospitalPharmacyManagementSystemDbContext(this.dbOptions, false);
+
+            this.dbContext.Database.EnsureCreated();
+            SeedDatabase(this.dbContext);
+
+            this.diseaseService = new DiseaseService(this.dbContext);
+        }
+    }
+}

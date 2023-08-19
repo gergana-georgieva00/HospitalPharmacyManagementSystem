0df94de5818c2d8c500aa651538120729f9ffaf8
Gergana Georgieva <gerganagergana1@gmail.com>
2023-08-19 13:38:10 +0300

    Added tests for GetFullNameByIdAsync

---
+++ HospitalPharmacyManagementSystem.Services.Tests/DrugServiceTests.cs
@@ -0,0 +1,32 @@
+﻿namespace HospitalPharmacyManagementSystem.Services.Tests
+{
+    using HospitalPharmacyManagementSystem.Data;
+    using HospitalPharmacyManagementSystem.Services.Data.Interfaces;
+    using HospitalPharmacyManagementSystem.Services.Data;
+    using Microsoft.EntityFrameworkCore;
+    using static DatabaseSeeder;
+
+    public class DrugServiceTests
+    {
+        private HospitalPharmacyManagementSystemDbContext dbContext;
+        private DbContextOptions<HospitalPharmacyManagementSystemDbContext> dbOptions;
+        private IDrugService drugService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<HospitalPharmacyManagementSystemDbContext>()
+                .UseInMemoryDatabase("HospitalPharmacyInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new HospitalPharmacyManagementSystemDbContext(this.dbOptions, false);
+
+            this.dbContext.Database.EnsureCreated();
+            SeedDatabase(this.dbContext);
+
+            this.drugService = new DrugService(this.dbContext);
+        }
+
+
+    }
+}
---
+++ HospitalPharmacyManagementSystem.Services.Tests/UserServiceTests.cs
@@ -0,0 +1,58 @@
+﻿namespace HospitalPharmacyManagementSystem.Services.Tests
+{
+    using HospitalPharmacyManagementSystem.Data;
+    using HospitalPharmacyManagementSystem.Services.Data.Interfaces;
+    using HospitalPharmacyManagementSystem.Services.Data;
+    using Microsoft.EntityFrameworkCore;
+    using static DatabaseSeeder;
+
+    public class UserServiceTests
+    {
+        private HospitalPharmacyManagementSystemDbContext dbContext;
+        private DbContextOptions<HospitalPharmacyManagementSystemDbContext> dbOptions;
+        private IUserService userService;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            this.dbOptions = new DbContextOptionsBuilder<HospitalPharmacyManagementSystemDbContext>()
+                .UseInMemoryDatabase("HospitalPharmacyInMemory" + Guid.NewGuid().ToString())
+                .Options;
+
+            this.dbContext = new HospitalPharmacyManagementSystemDbContext(this.dbOptions, false);
+
+            this.dbContext.Database.EnsureCreated();
+            SeedDatabase(this.dbContext);
+
+            this.userService = new UserService(this.dbContext);
+        }
+
+        [Test]
+        public async Task GetFullNameByIdAsyncShouldReturnCorrectResultWhenNull()
+        {
+            var user = await this.dbContext
+                .Users
+                .FirstOrDefaultAsync(u => u.Id.ToString() == PatientUser.FullName);
+
+            Assert.That(user, Is.EqualTo(null));
+        }
+
+        [Test]
+        public async Task GetFullNameByIdAsyncShouldReturnCorrectResult()
+        {
+            //var user = await this.dbContext
+            //    .Users
+            //    .FirstOrDefaultAsync(u => u.Id.ToString() == PharmacistUser.FullName);
+
+            //Assert.That(user.FullName, Is.EqualTo(PharmacistUser.FullName));
+
+            var user = await this.dbContext
+                .Users
+                .FirstOrDefaultAsync(u => u.Id == PharmacistUser.Id);
+
+            var result = await this.userService.GetFullNameByIdAsync(user.Id.ToString());
+
+            Assert.That(result, Is.EqualTo(PharmacistUser.FullName));
+        }
+    }
+}
